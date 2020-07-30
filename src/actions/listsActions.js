fc8bc89bd9703b1be10319857eb5e8f0e485d7e7
Sian Jyun Kuo <kuosianjyun@gmail.com>
2020-07-30 14:23:59 +0800

    update actions

--- src/actions/listsActions.js
+++ src/actions/listsActions.js
@@ -1,9 +1,14 @@
 import { CONSTANTS } from "../actions";
+import uuid from "uuidv4";
 
-export const addList = title => {
-  return {
-    type: CONSTANTS.ADD_LIST,
-    payload: title
+export const addList = (title) => {
+  return (dispatch, getState) => {
+    const boardID = getState().activeBoard;
+    const id = uuid();
+    dispatch({
+      type: CONSTANTS.ADD_LIST,
+      payload: { title, boardID, id },
+    });
   };
 };
 
@@ -15,15 +20,42 @@ export const sort = (
   draggableId,
   type
 ) => {
+  return (dispatch, getState) => {
+    const boardID = getState().activeBoard;
+    dispatch({
+      type: CONSTANTS.DRAG_HAPPENED,
+      payload: {
+        droppableIdStart,
+        droppableIdEnd,
+        droppableIndexEnd,
+        droppableIndexStart,
+        draggableId,
+        type,
+        boardID,
+      },
+    });
+  };
+};
+
+export const editTitle = (listID, newTitle) => {
   return {
-    type: CONSTANTS.DRAG_HAPPENED,
+    type: CONSTANTS.EDIT_LIST_TITLE,
     payload: {
-      droppableIdStart,
-      droppableIdEnd,
-      droppableIndexStart,
-      droppableIndexEnd,
-      draggableId,
-      type
-    }
+      listID,
+      newTitle,
+    },
+  };
+};
+
+export const deleteList = (listID) => {
+  return (dispatch, getState) => {
+    const boardID = getState().activeBoard;
+    return dispatch({
+      type: CONSTANTS.DELETE_LIST,
+      payload: {
+        listID,
+        boardID,
+      },
+    });
   };
 };
