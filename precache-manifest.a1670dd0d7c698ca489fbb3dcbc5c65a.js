fe798a7428d1d3be47168e014ee9a5974b6cda5a
Sian Jyun Kuo <kuosianjyun@gmail.com>
2020-07-31 11:38:38 +0800

    Updates

---
+++ precache-manifest.a1670dd0d7c698ca489fbb3dcbc5c65a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce5ab082de3257287c0e70111859d39",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "523dbd4a3d49e05ced9b",
+    "url": "/trello-clone/static/css/main.c3ddbcaa.chunk.css"
+  },
+  {
+    "revision": "e074fdd82fb230bb9103",
+    "url": "/trello-clone/static/js/2.92340612.chunk.js"
+  },
+  {
+    "revision": "97632a6e5444e62c56649563783e19ff",
+    "url": "/trello-clone/static/js/2.92340612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523dbd4a3d49e05ced9b",
+    "url": "/trello-clone/static/js/main.71052abc.chunk.js"
+  },
+  {
+    "revision": "01d9cced60e505c9249c",
+    "url": "/trello-clone/static/js/runtime-main.17cd50a1.js"
+  }
+]);
